fbbdf6a3c51a664c0da85ed887569b9f36b3ddb6
Zaine <74148219+rhinorunner@users.noreply.github.com>
2023-01-05 17:18:02 -0500

    Update sciolyDTB.ino

--- sciolyDTB.ino
+++ sciolyDTB.ino
@@ -1,9 +1,22 @@
+// need cmath for if the sensor changes logarithmicaly
+#include <cmath>
 
+// calibration variables, set up to work like
+// y = a(b(x+c))+d
+// y = a*log_b(x+c)+d  (forgot if this is how log parent function is)
+const static float CAL_A = 1;
+const static float CAL_B = 1;
+const static float CAL_C = 1;
+const static float CAL_D = 1;
 
-void setup() {
-  Serial.begin(9600);
+// pins
+// sensor pad in
+unsigned short PIN_PADIN = A0;
+
+void setup () {
+	Serial.begin(9600);
 }
 
-void loop() {
-  
+void loop () {
+	
 }
