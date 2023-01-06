fc02f1a206c9a29af0a73d84e6e1463bc7471fd3
Zaine <74148219+rhinorunner@users.noreply.github.com>
2023-01-06 08:44:56 -0500

    implemented the math part
    so when does "new_data" become true? when the sensor gets something? we might need some sort of "zero" button, i can implement that if you want. Also check lines 94 and 91.

--- sciolyDTB/sciolyDTB.ino
+++ sciolyDTB/sciolyDTB.ino
@@ -34,19 +34,19 @@ void IRAM_ATTR NewDataReadyISR() {
 // set up to work like this, where y = grams and x = sensor input
 // y = a(b(x+c))+d
 // y = a*log_b(x+c)+d  (forgot if this is how log parent function is)
-const static float CAL_A = 1;
-const static float CAL_B = 1;
-const static float CAL_C = 1;
-const static float CAL_D = 1;
+constexpr float CAL_A = 1;
+constexpr float CAL_B = 1;
+constexpr float CAL_C = 1;
+constexpr float CAL_D = 1;
 
 // PINS
 // sensor pad in
-unsigned short PIN_PADIN = A0;
+constexpr uint8_t PIN_PADIN = A0;
 
 //led pins
-int greenPin = 13;
-int redPin = 12;
-int bluePin = 11;
+constexpr uint8_t greenPin = 13;
+constexpr uint8_t redPin = 12;
+constexpr uint8_t bluePin = 11;
 
 void setup () {
   pinMode(greenPin,OUTPUT);
@@ -88,11 +88,18 @@ void loop () {
 	if (!new_data) {
     return;
   }
-
+  // does this not return float?
   int16_t results = ads.getLastConversionResults();
 
+  // whats this?
   Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(ads.computeVolts(results)); Serial.println("mV)");
 
+  // LINEAR
+  double grams = CAL_A * ( CAL_B * ( results + CAL_C ) ) + CAL_D;
+  // LOGARITHMIC
+  // idk if "log" is the log function from cmath.h
+  //double grams = CAL_A * ( log( results ) / log( CAL_B ) ) + CAL_D;
+  
   new_data = false;
 
   // In a real application we probably don't want to do a delay here if we are doing interrupt-based sampling, but we have a delay
