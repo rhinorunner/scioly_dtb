0a18a6f9fec2c7faf31a20b305959f0f06d9e876
MakerJake <69777180+MakerJake01@users.noreply.github.com>
2023-02-06 12:57:36 -0500

    fix math for conversion made sure to cast to float

--- sciolyDTB goat code for A team/sciolyDTB/sciolyDTB.ino
+++ sciolyDTB goat code for A team/sciolyDTB/sciolyDTB.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <math.h>
+#include <Math.h>
 
 //OLED Setup info
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
@@ -21,13 +21,17 @@ int avgVals[] = {0, 0, 0, 0, 0};
 #define bluePin 2
 
 #define buttonPin 7
-double tare = 0;
+double tare = 50;
 
 float e =  2.71828;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(buttonPin,INPUT);
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
   Serial.begin(9600);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -52,14 +56,14 @@ void loop() {
   // put your main code here, to run repeatedly:
   //read the ADC and then 
   smoothRead = smoothing(ads.readADC_SingleEnded(0));
-  Serial.println(smoothRead);
+  //Serial.println(smoothRead);
   //small delay so the mesurments are diffrent
   delay(10);
 
   //an easy way to zero out the scale
-  if(digitalRead(buttonPin) == HIGH){
-    tare = convertNumber(smoothRead);
-  }
+  //if(digitalRead(buttonPin) == HIGH){
+  //  tare = convertNumber(smoothRead);
+  //}
   
   //mostly boilerplate code to clear the display and then write text to the display
   display.clearDisplay();
@@ -68,16 +72,27 @@ void loop() {
   display.setCursor(0,0);
   display.print("mv ");
   display.println(ads.computeVolts(smoothRead)*1000);//display the reading from the adc without any changes
-  display.print("Gs ");
+  display.print("gs ");
   display.print(convertNumber(smoothRead)-tare);//find the weight but subtract what has been zeroed out
   display.display();
+
+  rangeLol(0, "b", 105, "r", 445, "g", 1000);
 }
 
 //returns the grams using the line of best fit
 double convertNumber(int data){
   //amazing conversion being done
+  //e^{((x/525)+(691/175))}
   double addedVal = 3.9486;
-  return exp((data/525)+addedVal);
+  float dataDiv = (float)data/525;
+  Serial.print(data);
+  Serial.print("    ");
+  Serial.print(dataDiv);
+  Serial.print("    ");
+  double eRaised = exp(dataDiv+addedVal);
+  Serial.println(eRaised);
+  return eRaised;
+  //return data;
 }
 
 //turns on the led of a certain color and turns the rest off
@@ -93,6 +108,12 @@ void greenLed() {
   digitalWrite(bluePin, LOW);
 }
 
+void ledOff(){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+}
+
 void blueLed() {
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, LOW);
@@ -100,7 +121,7 @@ void blueLed() {
 }
 
 //set the ranges for the leds to turn off and on
-void rangeLol(int bound1, char c1, int bound2, char c2, int bound3, char c3, int bound4){
+void rangeLol(int bound1, String c1, int bound2, String c2, int bound3, String c3, int bound4){
   double weight = convertNumber(smoothRead)-tare;
   if(bound1 < weight && weight < bound2){
     colorChoice(c1);
@@ -108,14 +129,17 @@ void rangeLol(int bound1, char c1, int bound2, char c2, int bound3, char c3, int
     colorChoice(c2);
   }else if(bound3 < weight && weight < bound4){
     colorChoice(c3);
+  }else{
+    ledOff();
   }
 }
 
 //parse the color choosen
-void colorChoice(char color){
+void colorChoice(String color){
   if(color == "r"){
     redLed();
   }else if(color == "g"){
+    Serial.println(color);
     greenLed();
   }else if(color == "b"){
     blueLed();
